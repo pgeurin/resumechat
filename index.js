3f2c117bdbb9b16f6025402c2928252443c3a534
Philip Geurin <philip.geurin@gmail.com>
2017-02-03 13:30:07 -0800

    fix socket.io to use new moving heroku sockets

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -29,3 +30,37 @@ io.on('connection', function(socket){
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+*/
+
+//Used parts of https://github.com/heroku-examples/node-socket.io/blob/master/server.j-s
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'views/pages/index.html');
+//const PHOTO = path.join(__dirname, 'views/pages/PhilGeurinHeadshot3RotatedResized2.jpg');
+
+
+const server = express()
+  .get('/PhilGeurinHeadshot3RotatedResized2.jpg', function(req, res){res.sendfile('views/pages/PhilGeurinHeadshot3RotatedResized2.jpg');})
+  .use((req, res) => res.sendFile(INDEX) )
+  //.use((req, res) => res.sendFile(PHOTO) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
